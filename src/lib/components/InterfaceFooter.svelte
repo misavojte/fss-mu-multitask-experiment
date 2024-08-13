9ee2d1cc91ec68873298902bbf3ced58bec5e744
Michaela Vojtechovska <mich.vojtechovska@gmail.com>
2024-08-13 15:46:35 +0200

    chore: Update InterfaceFooter component to use relative path for logo image

--- src/lib/components/InterfaceFooter.svelte
+++ src/lib/components/InterfaceFooter.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
 	import LL from '../../i18n/i18n-svelte';
-	import { base } from '$app/paths';
 
 	const currentYear = new Date().getFullYear();
 </script>
 
 <footer class="text-neutral-600 body-font border-t border-neutral-200">
 	<div class="container mx-auto py-4 px-5 gap-4 flex flex-wrap flex-col sm:flex-row">
-		<img src="{base}/logo.png" alt="Logo" class="h-8 w-auto" />
+		<img src="logo.png" alt="Logo" class="h-8 w-auto" />
 		<p class="text-xs text-neutral-500 sm:ml-auto sm:mt-0 mt-2 sm:text-left">
 			{$LL.footerText()}
 		</p>
