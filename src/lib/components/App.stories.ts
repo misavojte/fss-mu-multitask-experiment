3f69f6452c59751c2b9fca88308696993536d0fb
Michaela Vojtechovska <mich.vojtechovska@gmail.com>
2024-09-19 14:40:01 +0200

    :bug Add locale to Storybook

--- src/lib/components/App.stories.ts
+++ src/lib/components/App.stories.ts
@@ -1,5 +1,10 @@
 import type { Meta, StoryObj } from '@storybook/svelte';
 import App from './App.svelte';
+import { setLocale } from '../../i18n/i18n-svelte';
+import { loadLocale } from '../../i18n/i18n-util.sync';
+
+loadLocale('cs');
+setLocale('cs');
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 const meta = {
