3d84c71a34ef8a36a0797da86b3d8c98c215705e
Michaela Vojtechovska <mich.vojtechovska@gmail.com>
2024-11-01 10:14:55 +0100

    Refactor task handler imports to use TaskHandlerIntelligenceIDB across multiple routes

--- src/routes/even/+page.svelte
+++ src/routes/even/+page.svelte
@@ -2,14 +2,14 @@
 	import App from '$lib/components/App.svelte';
 	import { ConnectLoggerIDB } from '$lib/services/ConnectLoggerIDB';
 	import { GazeSaverIDB } from '$lib/services/GazeSaverIDB';
-	import { TaskPatternMatchingHandlerIDB } from '$lib/services/TaskPatternMatchingHandlerIDB';
+	import { TaskHandlerIntelligenceIDB } from '$lib/services/TaskHandlerIDB';
 	import { TimestampQuestionServiceIDB } from '$lib/services/TimestampQuestionServiceIDB';
 
 	const sessionId = new Date().getTime() + '-' + Math.floor(1000 + Math.random() * 9000);
 	const questionsService = new TimestampQuestionServiceIDB(sessionId);
-	const taskHandler = new TaskPatternMatchingHandlerIDB(sessionId);
+	const taskHandler = new TaskHandlerIntelligenceIDB(sessionId);
 	const connectLogger = new ConnectLoggerIDB(sessionId);
 	const gazeSaver = new GazeSaverIDB(sessionId);
 </script>
 
-<App {gazeSaver} {connectLogger} {questionsService} {taskHandler} variant="even" />
+<App {gazeSaver} {connectLogger} {questionsService} {taskHandler} />
