5699901fe8ecf10d98a8ac90ea0e4b2b90fcd188
Michaela Vojtechovska <mich.vojtechovska@gmail.com>
2024-11-01 21:53:51 +0100

    Add AnimationTarget component and integrate with TaskDocumentary for feedback on clicks

---
+++ src/lib/components/AnimationTarget.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import { fade } from 'svelte/transition';
+
+	export let centerCoordinates: { x: number; y: number };
+	export let color: string = 'red';
+	export let content: string = '+1';
+	export let duration: number = 1000;
+</script>
+
+<div
+	transition:fade
+	class="slowly-moving-up absolute z-50 pointer-events-none flex justify-center items-center text-white text-lg font-bold text-center p-6 rounded-full bg-opacity-50 {color ===
+	'green'
+		? 'bg-green-500'
+		: 'bg-red-500'}"
+	style="left: {centerCoordinates.x}px; top: {centerCoordinates.y}px; transform: translate(-50%, -50%); --duration: {duration}ms;"
+>
+	{content}
+</div>
+
+<style>
+	@keyframes slowly-moving-up {
+		0% {
+			transform: translate(-50%, -50%) translateY(0);
+			opacity: 0;
+		}
+		25% {
+			transform: translate(-50%, -50%) translateY(-2px);
+			opacity: 1;
+		}
+		50% {
+			transform: translate(-50%, -50%) translateY(-5px);
+			opacity: 1;
+		}
+		100% {
+			transform: translate(-50%, -50%) translateY(-10px);
+			opacity: 0;
+		}
+	}
+	.slowly-moving-up {
+		animation: slowly-moving-up var(--duration) linear forwards;
+	}
+</style>
--- src/lib/components/TaskDocumentary.svelte
+++ src/lib/components/TaskDocumentary.svelte
@@ -1,13 +1,24 @@
 <script lang="ts">
-	import { createEventDispatcher } from 'svelte';
+	import { createEventDispatcher, onDestroy, SvelteComponent } from 'svelte';
 	import InterfaceFrame from './InterfaceFrame.svelte';
+	import { waitForTimeoutCancellable } from '$lib/utils/waitForCondition';
+	import TaskDocumentaryButton from './TaskDocumentaryButton.svelte';
+	import AnimationTarget from './AnimationTarget.svelte';
 
 	export let videoDocumentarySrc: string;
 	export let width: number = 400;
 	export let height: number = 300;
-	export let hideAllControls: boolean = false;
+	export const hideAllControls: boolean = true;
 	export let play: boolean = false;
 	export let muted: boolean = false;
+	export let wordOccurence: string = 'SLOVO';
+	export let wordOccurenceTolerance: number = 10000;
+	export let wordOccurenceTimestamps: number[] = [7000, 7100];
+
+	const wordOccurenceTimestampCheck: {
+		time: number;
+		checked: boolean;
+	}[] = wordOccurenceTimestamps.map((time) => ({ time, checked: false }));
 
 	$: handlePlayChange(play);
 
@@ -22,8 +33,67 @@
 	};
 
 	let video: HTMLVideoElement | null = null;
+	let feedbackCircles: Record<string, SvelteComponent> = {};
 
 	const dispatch = createEventDispatcher();
+	let abortController = new AbortController();
+	console.log('TaskDocumentary', wordOccurenceTimestampCheck);
+
+	const handleClick = async (e: CustomEvent<MouseEvent>) => {
+		const wasCorrect = evaluateCorrectnessOfClick();
+		if (wasCorrect) {
+			dispatch('correct');
+		} else {
+			dispatch('incorrect');
+		}
+		const randomId = Math.random().toString();
+		feedbackCircles[randomId] = new AnimationTarget({
+			target: document.body,
+			props: {
+				centerCoordinates: { x: e.detail.clientX, y: e.detail.clientY },
+				color: wasCorrect ? 'green' : 'red',
+				content: wasCorrect ? '+ 1 bod' : 'špatně'
+			}
+		});
+		try {
+			await waitForTimeoutCancellable(1000, abortController.signal);
+		} catch (e) {
+			// Do nothing
+		}
+		feedbackCircles[randomId].$destroy();
+		delete feedbackCircles[randomId];
+	};
+
+	const evaluateCorrectnessOfClick = () => {
+		if (!video) return false;
+		const toleranceTimeEnd = video.currentTime * 1000;
+		const toleranceTimeStart = toleranceTimeEnd - wordOccurenceTolerance; // So if video is at 7.1s and tolerance is 1000ms, it will check from 6.1s to 7.1s
+
+		const timestampChecksWithinTolerance = wordOccurenceTimestampCheck.filter(
+			(timestampCheck) =>
+				timestampCheck.time >= toleranceTimeStart && timestampCheck.time <= toleranceTimeEnd
+		);
+
+		if (timestampChecksWithinTolerance.length === 0) return false;
+
+		const correctTimestampCheck = timestampChecksWithinTolerance.find(
+			(timestampCheck) => !timestampCheck.checked
+		);
+		if (!correctTimestampCheck) return false;
+
+		correctTimestampCheck.checked = true;
+		return true;
+	};
+
+	let startTime: number | null = null;
+	const handleStart = () => {
+		dispatch('started');
+		startTime = Date.now();
+	};
+
+	onDestroy(() => {
+		abortController.abort();
+	});
 </script>
 
 <InterfaceFrame {width} {height} showBezels={false}>
@@ -41,8 +111,10 @@
 			on:canplay={() => {
 				dispatch('loaded');
 			}}
+			on:play={handleStart}
 		>
 			<track kind="captions" src={videoDocumentarySrc} srclang="en" label="English" default />
 		</video>
+		<TaskDocumentaryButton {wordOccurence} on:click={handleClick} />
 	</div>
 </InterfaceFrame>
