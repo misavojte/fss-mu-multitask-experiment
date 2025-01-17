264942ff52a81e6255935bdea7bb4f37af246c33
misavojte <mich.vojtechovska@gmail.com>
2025-01-17 15:35:02 +0100

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,70 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			includeAssets: ['favicon.png'],
+			manifest: {
+				name: 'FSS MU Multitask Experiment',
+				short_name: 'FSS MU MTExp',
+				description: 'FSS MU Multitask Experiment Application',
+				theme_color: '#ffffff',
+				icons: [
+					{
+						src: 'logo.png',
+						sizes: '192x192',
+						type: 'image/png'
+					},
+					{
+						src: 'logo.png',
+						sizes: '512x512',
+						type: 'image/png'
+					},
+					{
+						src: 'logo.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'any maskable'
+					}
+				]
+			},
+			workbox: {
+				globPatterns: ['**/*.{js,css,html,ico,png,svg,webp,jpg,jpeg}'],
+				runtimeCaching: [
+					{
+						urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+						handler: 'CacheFirst',
+						options: {
+							cacheName: 'google-fonts-cache',
+							expiration: {
+								maxEntries: 10,
+								maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+							},
+							cacheableResponse: {
+								statuses: [0, 200]
+							}
+						}
+					},
+					{
+						urlPattern: /^https:\/\/fonts\.gstatic\.com\/.*/i,
+						handler: 'CacheFirst',
+						options: {
+							cacheName: 'gstatic-fonts-cache',
+							expiration: {
+								maxEntries: 10,
+								maxAgeSeconds: 60 * 60 * 24 * 365 // <== 365 days
+							},
+							cacheableResponse: {
+								statuses: [0, 200]
+							}
+						}
+					}
+				]
+			}
+		})
+	]
 });
